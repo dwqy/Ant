eebb89a66960e9c6b0fe06c2f70f4db3abb8b3a0
wqy <wqy@wqydeMBP.lan>
2020-03-05 00:59:37 +0800

    mac

---
+++ 1-15方法的定义和使用/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_15方法的定义和使用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
