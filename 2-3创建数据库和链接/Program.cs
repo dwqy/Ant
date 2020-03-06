47bb93e203359332ad8c9cacd46b29c1d8cdef7e
dwqy <53160781+dwqy@users.noreply.github.com>
2020-03-06 18:33:07 +0800

    3.6win

--- 2-3创建数据库和链接/Program.cs
+++ 2-3创建数据库和链接/Program.cs
@@ -6,6 +6,11 @@ namespace _2_3创建数据库和链接
     {
         static void Main(string[] args)
         {
+            /*
+             * 
+             * 1视图 sql server对象资源管理器 中创建数据库和表 设计
+             * 2 视图  服务器资源管理器
+             */
             Console.WriteLine("Hello World!");
         }
     }
